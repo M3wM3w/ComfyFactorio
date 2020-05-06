a0c20cd222d3f7853d2024afed7ab6ae3b1c62b6
danielmartin0 <danielmartin111@gmail.com>
2020-05-06 01:21:51 +0100

    debug fix

--- maps/chronosphere/main.lua
+++ maps/chronosphere/main.lua
@@ -725,15 +725,15 @@ if _DEBUG then
 end
 --Time for the debug code.  If any (not global.) globals are written to at this point, an error will be thrown.
 --eg, x = 2 will throw an error because it's not global.x or local x
-setmetatable(_G, {
-    __newindex = function(_, n, v)
-        log("Desync warning: attempt to write to undeclared var " .. n)
-        -- game.print("Attempt to write to undeclared var " .. n)
-        global[n] = v;
-    end,
-    __index = function(_, n)
-        return global[n];
-    end
-})
+--setmetatable(_G, {
+--    __newindex = function(_, n, v)
+--        log("Desync warning: attempt to write to undeclared var " .. n)
+--        -- game.print("Attempt to write to undeclared var " .. n)
+--        global[n] = v;
+--    end,
+--    __index = function(_, n)
+--        return global[n];
+--    end
+--})
 
 return Public
