d8732026a5038810590cb016badbfb369c2e2ace
danielmartin0 <danielmartin111@gmail.com>
2020-05-16 13:18:03 +0100

    balance

--- maps/chronosphere/balance.lua
+++ maps/chronosphere/balance.lua
@@ -537,7 +537,7 @@ function Public.treasure_chest_loot(difficulty, planet)
 end
 
 function Public.scrap_quantity_multiplier(evolution_factor, mining_drill_productivity_bonus)
-	return 1 + 4 * evolution_factor --removed dependence on mining drill tech bonus to nerf tech slightly and make the map more distinctive
+	return 0.6 + 2.4 * evolution_factor --removed dependence on mining drill tech bonus to nerf tech slightly and make the map more distinctive
 end
 
 Public.scrap_yield_amounts = {
